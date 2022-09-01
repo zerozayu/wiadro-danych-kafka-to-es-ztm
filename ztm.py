126f12899279bfb9feb55c55a6472f0a6eecd8ef
zerozayu <304307556@qq.com>
2022-09-01 16:45:52 +0800

    fix: 改了一些自己的配置

--- ztm.py
+++ ztm.py
@@ -1,4 +1,4 @@
-import requests 
+import requests
 import json
 import time
 from kafka import KafkaProducer
@@ -9,24 +9,25 @@ resource_id = 'f2e5503e927d-4ad3-9500-4ab9e55deb59'
 sleep_time = 15
 
 bus_params = {
-    'apikey':token,
-    'type':1,
+    'apikey': token,
+    'type': 1,
     'resource_id': resource_id
-    }
+}
 tram_params = {
-    'apikey':token,
-    'type':2,
+    'apikey': token,
+    'type': 2,
     'resource_id': resource_id
-    }
+}
 
 while True:
     try:
-        r = requests.get(url = url, params = bus_params)
-        data = r.json() 
+        print("发起请求")
+        r = requests.get(url=url, params=bus_params)
+        data = r.json()
         producer = KafkaProducer(bootstrap_servers=['localhost:29092'],
-                                value_serializer=lambda x: json.dumps(x).encode('utf-8'),
-                                key_serializer=lambda x: x
-                                )
+                                 value_serializer=lambda x: json.dumps(x).encode('utf-8'),
+                                 key_serializer=lambda x: x
+                                 )
 
         print('Sending records...')
         for record in data['result']:
